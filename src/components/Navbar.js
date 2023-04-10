b2b832e9f7903c9a423ace2787f3cfe717453f47
isaacg <iwanyoike65@gmail.com>
2023-04-10 16:51:16 +0300

    Add empty navbar/ Add linters and do linter checks

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+// import { NavLink } from "react-router-dom";
+
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="links">
+      {/* <NavLink to="/"></NavLink>
+            <NavLink to="/Missions">Missions</NavLink>
+            <NavLink to="/Myprofile">Myprofile</NavLink> */}
+    </div>
+  </nav>
+);
+
+export default Navbar;
