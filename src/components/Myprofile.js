ba5d316e4022b8cae3e642a47cac1c30697857ee
Enis <enis.mema@gmail.com>
2023-04-10 16:27:48 +0200

    Create routes and build basic components

---
+++ src/components/Myprofile.js
@@ -0,0 +1,7 @@
+const Myprofile = () => (
+  <div>
+    <h1>My profile</h1>
+  </div>
+);
+
+export default Myprofile;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
-// import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => (
   <nav className="navbar">
     <div className="links">
-      {/* <NavLink to="/"></NavLink>
-            <NavLink to="/Missions">Missions</NavLink>
-            <NavLink to="/Myprofile">Myprofile</NavLink> */}
+      <NavLink to="/">Rockets</NavLink>
+      <NavLink to="Missions">Missions</NavLink>
+      <NavLink to="Myprofile">Myprofile</NavLink>
     </div>
   </nav>
 );
