dafcab93fcd2de059c3e9eb00fc8d95356122bd9
isaacg <iwanyoike65@gmail.com>
2023-04-11 13:34:41 +0300

    display and add styling to booked rockets in myprofile

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,7 +1,28 @@
-const Myprofile = () => (
-  <div>
-    <h1>My profile</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+import './myprofile.css';
+
+const Myprofile = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+
+  return (
+    <div className="my-profile-div">
+      <div>
+        <h2 className="my-profile-h2">My missions</h2>
+      </div>
+      <div className="mprdiv">
+        <h2 className="my-profile-h2">My rockets</h2>
+        <table className="mprtable">
+          <tbody>
+            {rockets.map((rocket) => (rocket.reserved ? (
+              <tr key={rocket.id}>
+                <td className="mprtd">{rocket.rocket_name}</td>
+              </tr>
+            ) : null))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Myprofile;
