5dd6ae5f16f23649d77fb825d0d1300e2060e5be
isaacg <iwanyoike65@gmail.com>
2023-04-11 18:04:58 +0300

    Add reocket tests and change styling

--- src/tests/__snapshots__/components.test.js
+++ src/tests/__snapshots__/components.test.js
@@ -1,18 +1,26 @@
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import {
+  render,
+  screen, fireEvent, within,
+} from '@testing-library/react';
 import { Provider } from 'react-redux';
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
 import store from '../../redux/store';
 import Missions from '../../components/Missions';
 import Navbar from '../../components/Navbar';
 import Header from '../../components/Header';
 import Myprofile from '../../components/Myprofile';
 
+import '@testing-library/jest-dom/extend-expect';
+import Rockets from '../../components/Rockets';
+
 describe('Render All Components', () => {
   test('render navbar', () => {
     const { container } = render(
       <MemoryRouter>
         <Navbar />
-      </MemoryRouter>
+      </MemoryRouter>,
     );
     expect(container.firstChild).toMatchSnapshot(`
     <div className="links">
@@ -29,7 +37,7 @@ describe('Render All Components', () => {
         <Provider store={store}>
           <Header />
         </Provider>
-      </MemoryRouter>
+      </MemoryRouter>,
     );
     expect(container.firstChild).toMatchSnapshot(`<div className="header">
     <div className="header-1">
@@ -46,7 +54,7 @@ describe('Render All Components', () => {
         <Provider store={store}>
           <Missions />
         </Provider>
-      </MemoryRouter>
+      </MemoryRouter>,
     );
     expect(container.firstChild).toMatchSnapshot(`
         <table>
@@ -102,7 +110,7 @@ describe('Render All Components', () => {
         <Provider store={store}>
           <Myprofile />
         </Provider>
-      </MemoryRouter>
+      </MemoryRouter>,
     );
     expect(container.firstChild).toMatchSnapshot(`
     <div className="my-profile-div">
@@ -127,5 +135,85 @@ describe('Render All Components', () => {
         </table>
       </div>
     </div>`);
+  });
+});
+
+const mockStore = configureMockStore();
+
+describe('Rockets', () => {
+  const initialState = {
+    rockets: {
+      rockets: [
+        {
+          id: '1',
+          rocket_name: 'Rocket 1',
+          description: 'Rocket 1 description',
+          flickr_images: ['https://example.com/rocket1.jpg'],
+          reserved: false,
+        },
+        {
+          id: '2',
+          rocket_name: 'Rocket 2',
+          description: 'Rocket 2 description',
+          flickr_images: ['https://example.com/rocket2.jpg'],
+          reserved: false,
+        },
+        {
+          id: '3',
+          rocket_name: 'Rocket 3',
+          description: 'Rocket 3 description',
+          flickr_images: ['https://example.com/rocket2.jpg'],
+          reserved: true,
+        },
+      ],
+      fetched: true,
+    },
+  };
+
+  let store;
+
+  beforeEach(() => {
+    store = mockStore(initialState);
+    store.clearActions();
+  });
+
+  test('renders Rockets component', () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    expect(screen.getByRole('heading', { name: /Rocket 1/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /Rocket 2/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /Rocket 3/i })).toBeInTheDocument();
+  });
+
+  test('reserves a rocket', () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    const rocketElement = screen.getByTestId('rocket-1');
+    const reserveButton = within(rocketElement).getByText(/Reserve Rocket/i);
+    fireEvent.click(reserveButton);
+
+    expect(store.getActions()).toEqual([{ type: 'rockets/bookRockets', payload: '1' }]);
+  });
+
+  test('cancels reservation', () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    const rocketElement = screen.getByTestId('rocket-3');
+    const cancelButton = within(rocketElement).getByText(/Cancel Reservation/i);
+    fireEvent.click(cancelButton);
+
+    expect(store.getActions()).toEqual([{ type: 'rockets/unBookRockets', payload: '3' }]);
   });
 });
