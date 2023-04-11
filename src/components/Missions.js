7da94fa0e53d4b0801facf5b6d05df9bab66c776
Enis <enis.mema@gmail.com>
2023-04-11 09:56:34 +0200

    Fetch and render rockets

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,37 @@
-const Missions = () => (
-  <div>
-    <h1>Missions</h1>
-  </div>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missionsSlice';
+
+const Missions = () => {
+  const { missions } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {missions.map((mission) => (
+          <tr className="mission-list" key={mission.mission_id}>
+            <td className="mission-title">{mission.mission_name}</td>
+            <td className="mission-description">{mission.description}</td>
+            <td className="mission-button">
+              <button id={mission.mission_id} type="button">
+                Join mission
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Missions;
